281ae5409279369d91463e47d8cd32b6d3133987
jaybo1001 <138844854+jaybo1001@users.noreply.github.com>
2024-09-29 13:27:49 -0700

    chore: Update next.config.mjs to remove unused code

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,4 @@
-import { NextFederationPlugin } from '@module-federation/nextjs-mf';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config, options) {
-    const { isServer } = options;
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: 'app1',
-        filename: 'static/chunks/remoteEntry.js',
-        exposes: {
-          './Page': './app/page.tsx',
-        },
-        shared: {
-          react: {
-            singleton: true,
-            eager: true,
-            requiredVersion: false,
-          },
-          'react-dom': {
-            singleton: true,
-            eager: true,
-            requiredVersion: false,
-          },
-        },
-        extraOptions: {
-          exposePages: true,
-        },
-      })
-    );
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
